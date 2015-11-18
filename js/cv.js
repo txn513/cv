8d09e29f4df931cbc7a79777edd7818c5ec5771f
txn513 <xtian513@gmail.com>
2015-11-19 03:01:07 +0800

    updated skill section

--- js/cv.js
+++ js/cv.js
@@ -1,7 +1,13 @@
 $(function(){
-	var $clientWidth = parseInt($('html').css('width'));
-	var $right_wrap = $('.right_wrap');
-	if($clientWidth < 768){
-		$right_wrap.append($('.contact'));
-	};
+	(function moveElement(){
+		var $clientWidth = parseInt($('html').css('width'));
+		var $right_wrap = $('.right_wrap');
+		if($clientWidth < 768){
+			$right_wrap.append($('.contact'));
+		};
+	})();
+
+	(function(){
+		
+	})();
 });
