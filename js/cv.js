6e5ab21d179c36fb2c854327f1563bbbf791bef8
txn513 <xtian513@gmail.com>
2016-04-26 01:44:38 +0800

    added project

--- js/cv.js
+++ js/cv.js
@@ -2,17 +2,47 @@ $(function(){
 	(function moveElement(){
 		var $clientWidth = parseInt($('html').css('width'));
 		var $right_wrap = $('.right_wrap');
+		var progessBarScroll = $('.last').offset().top - $(window).height();
+		var animation_1 = $('.animation-1').offset().top - $(window).height();
 		if($clientWidth < 768){
-			$right_wrap.append($('.contact'));
+			$right_wrap.append($('.contact'));	
 		};
-	})();
 
+		animationStart();
+
+		function animationStart(){
+			if ($(window).scrollTop() > progessBarScroll){
+				animateProgessBar();
+			}
+			else{
+				mobileScroll();
+			}
+			if ($(window).scrollTop() > animation_1){
+				alert(1);
+			}
+			else{
+				
+			}
+		}
+		function mobileScroll(){
+			$(window).bind('scroll', function scrollTop(){
+				if($(window).scrollTop() > progessBarScroll){
+					animateProgessBar();
+					//alert(1);
+					$(this).unbind();
+				};
+			});	
+		}
+		function animateProgessBar(){
+			$('.progress-bar').each(function() {
+			    var bar_value = $(this).attr('aria-valuenow') + '%';                
+			    $(this).animate({ width: bar_value }, 'slow');
+			});
+		}
+		function animateTitle1(){
+			
+		}
+		
+	})();
 	
-
-	(function(){
-		$('.progress-bar').each(function() {
-		    var bar_value = $(this).attr('aria-valuenow') + '%';                
-		    $(this).animate({ width: bar_value }, 2000);
-		});
-	})();
 });
